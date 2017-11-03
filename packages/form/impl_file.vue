c0d551accbfa44c6e968dbd773a85d50c44b8523
charlie <xyhp915@qq.com>
2017-11-03 16:14:05 +0800

    update .

--- packages/form/impl_file.vue
+++ packages/form/impl_file.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="v-control">
-    <div class="v-file">
-      hello v-file
-    </div>
+    <input type="file"
+           class="v-file v-single-file-input"
+           name="file"
+           :id="labelId"
+           @change="_handleFileChange"
+           :accept="acceptTypes"
+    >
   </div>
 </template>
-
 <script>
   export default {
-    name: 'VFile'
+    props: {
+      acceptTypes: {
+        type: String,
+        'default': '.gif,.jpeg,.jpg,.png'
+      },
+
+      labelId: {
+        type: String,
+        'default': `v-file-input-${Date.now()}`
+      }
+    },
+    data () {
+      return {}
+    },
+    methods: {
+      _handleFileChange: function (e) {
+        this.$emit('on-file-change', e.target.files[0])
+        e.target.value = ''
+      }
+    }
   }
 </script>
